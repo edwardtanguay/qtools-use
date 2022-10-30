6ad7db4f2ff039f43c01c605cdc8e58f882d5fa2
Edward Tanguay (windows) <edward@tanguay.info>
2022-10-30 18:17:52 +0100

    init commit

---
+++ qxml.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import * as qsys from './qsys.js';
+import { XMLParser } from 'fast-xml-parser';
+
+/**
+ * Convert XML file to JavaScript object.
+ *
+ * const obj = qxml.getXmlFileAsObject('src/data/settings.xml');
+ *
+ * {
+ *   settings: {
+ *     type: 'jdbc-mysql',
+ *     user: 'sbanks',
+ *     password: '123',
+ *   }
+ * }
+ */
+export const getXmlFileAsObject = (pathAndFileName) => {
+    const fullPathAndFileName =
+        qsys.buildSystemAbsolutePathAndFileName(pathAndFileName);
+    const xmlAsText = fs.readFileSync(fullPathAndFileName, 'utf8');
+    const parser = new XMLParser();
+    const xmlAsObject = parser.parse(xmlAsText);
+    return xmlAsObject;
+};
